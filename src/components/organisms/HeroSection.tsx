4261907605598b5f553d0a96288386444ff96e80
1478952 <1478952@naver.com>
2024-07-10 22:31:58 +0900

    메인 수정

--- src/components/organisms/HeroSection.tsx
+++ src/components/organisms/HeroSection.tsx
@@ -2,14 +2,11 @@ import { MAIN_TEXTS } from "@/common/constants/data";
 import AnimationTitle from "../atoms/AnimationTitle";
 import ContainedButton from "../atoms/ContainedButton";
 import Description from "../atoms/Description";
-import BottomBlurEffect from "../atoms/effects/BottomBlurEffect";
-import TopBlurEffect from "../atoms/effects/TopBlurEffect";
 import Image from "next/image";
 
 const HeroSection = () => {
   return (
-    <section className="relative isolate px-6 pt-14 lg:px-8">
-      <TopBlurEffect />
+    <section className="relative isolate px-6p pt-10 lg:px-8">
       <Image
         src={"/assets/images/profile.png"}
         width={80}
@@ -17,10 +14,10 @@ const HeroSection = () => {
         alt="profile"
         className="absolute top-48 left-32"
       />
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div className="mx-auto max-w-2xl py-24">
         <div className="text-center">
           <AnimationTitle animationTexts={MAIN_TEXTS} />
-          <Description text="반가워요! FrontEnd Developer 김정운입니다." />
+          <Description text="안녕하세요!  FrontEnd Developer 김정운입니다." />
 
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <ContainedButton text="Get started" />
@@ -28,12 +25,11 @@ const HeroSection = () => {
               href="#"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
-              Learn more <span aria-hidden="true">→</span>
+              더 알아봐요! <span aria-hidden="true">→</span>
             </a>
           </div>
         </div>
       </div>
-      <BottomBlurEffect />
     </section>
   );
 };
--- src/components/templates/MainTemplate.tsx
+++ src/components/templates/MainTemplate.tsx
@@ -1,11 +1,19 @@
+import Link from "next/link";
 import Header from "../organisms/Header";
 import HeroSection from "../organisms/HeroSection";
+import GithubIcon from "../atoms/icons/GithubIcon";
 
 // 5138BE
 const MainTemplate = () => {
   return (
     <main className="bg-blue-600">
       <Header />
+      <Link
+        href={"https://github.com/1478952"}
+        className="absolute right-0 translate-x-8 top-20 z-10"
+      >
+        <GithubIcon width="4rem" height="4rem" />
+      </Link>
       <HeroSection />
     </main>
   );
